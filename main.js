aae8d64d8f0387a158ecfa5329f20b5f3d404fbc
“Mrinmoyee <mrmishra@ucsc.edu>
2022-10-12 23:01:00 -0700

    added 2 falling enemies, working on time event

--- main.js
+++ main.js
@@ -3,7 +3,16 @@ title = "TWO COLOR";
 description = ` 
 `;
 
-characters = [];
+characters = [
+`
+  rr
+  rr
+  rr
+rrrrrr
+ rrrr
+  rr
+`
+];
 
     
 const G = {
@@ -26,10 +35,15 @@ options = {
   * @type  { Enemy [] }
   */
   let enemy;
+  let enemy2;
+
   let player;
   let c;
   let state;
 
+  let timedEvent = false;
+  let time = 0;
+  let start = 0;
 
 function update() {
   if (!ticks) {
@@ -40,6 +54,15 @@ function update() {
         pos: vec(G.WIDTH / 2, 3 * G.HEIGHT / 4)
     };
     enemy = times(10, () => {
+      const posX = rnd(G.WIDTH/2, G.WIDTH); 
+      const posY = rnd(0, G.HEIGHT);
+      return {
+          pos: vec(posX, posY),
+          speed: rnd(0.8, 1.5)
+      };
+    });
+      
+    enemy2 = times(10, () => {
       const posX = 0; 
       const posY = rnd(0, G.HEIGHT);
       return {
@@ -47,9 +70,17 @@ function update() {
           speed: rnd(0.8, 1.5)
       };
     });
+      
     for (let i = 0; i < 10; i++) {
       enemy.push({
           pos: vec(rnd(G.WIDTH * (2/3), G.WIDTH), rnd(0, G.HEIGHT)),
+          speed: rnd(0.8, 1.5)
+      });
+    }
+      
+    for (let i = 0; i < 10; i++) {
+      enemy2.push({
+          pos: vec(rnd(0, G.WIDTH * (1/3)), rnd(0, G.HEIGHT)),
           speed: rnd(0.8, 1.5)
       });
     }
@@ -59,15 +90,31 @@ function update() {
     e.pos.y += e.speed;
     e.pos.wrap(0, G.WIDTH, 0, G.HEIGHT);
     color("red");
-    box(e.pos, 1);
+    char("a", e.pos);
   });
     
-  if (ticks == 180) {
-      remove(enemy, (e) => {
-          return true;
-      })
-      ticks = 0;
-  }
+  enemy2.forEach((e) => {
+    e.pos.y += e.speed;
+    e.pos.wrap(0, G.WIDTH, 0, G.HEIGHT);
+    color("blue");
+    char("a", e.pos);
+  });
+    
+// timed event
+//  if (timedEvent == false) {
+//      start = 60;
+//      time = start;
+//      timedEvent = true;
+//  } 
+//  if ((timedEvent == true) && (ticks == 60)) {
+//      time-=1;
+//      enemy.forEach((e) => {
+//        color("green");
+//        char("a", e.pos);
+//      });
+//    
+//      
+//  }
 
   //color change
   if (input.isJustPressed) {
